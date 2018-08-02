4751f87d2018e8e86aef8bd5a0621a0a6e972123
Tomáš Bláha <tomas.blaha@driveto.cz>
2018-08-02 17:11:08 +0200

    Check class for annotations when creating list

--- src/ElasticOdm/AnnotatedClass/ClassListGetter.php
+++ src/ElasticOdm/AnnotatedClass/ClassListGetter.php
@@ -6,7 +6,8 @@ namespace ElasticOdm\AnnotatedClass;
 
 use ElasticOdm\AnnotatedClass\Exception\NoClassException;
 use ElasticOdm\AnnotatedClass\Exception\NoNamespaceException;
-use ReflectionException;
+use ElasticOdm\Annotation\PropertyAnnotationResolver;
+use ElasticOdm\Serializer\MappingSerializer;
 use SplFileInfo;
 use Symfony\Component\Finder\Finder;
 
@@ -18,10 +19,16 @@ class ClassListGetter
     /** @var \ElasticOdm\AnnotatedClass\PhpFileParser */
     private $phpFileParser;
 
+    /** @var \ElasticOdm\Serializer\MappingSerializer */
+    private $mappingSerializer;
+
     public function __construct()
     {
         $this->finder = new Finder();
         $this->phpFileParser = new PhpFileParser();
+	    $this->mappingSerializer = new MappingSerializer(
+	    	new PropertyAnnotationResolver()
+	    );
     }
 
     public function getClassesList(string $sourceFolderPath): ClassList
@@ -40,12 +47,13 @@ class ClassListGetter
 
                 $fqcn = $this->phpFileParser->buildFqcnFromClassAndNamespace($namespace, $classname);
 
-                new \ReflectionClass($fqcn);
+                $attributes = $this->mappingSerializer->serialize($fqcn);
+
+                var_dump($attributes);
 
                 $classes[] = $fqcn;
             } catch (NoClassException $e) {
             } catch (NoNamespaceException $e) {
-            } catch (ReflectionException $e) {
             }
         }
 
--- tests/fixtures/AnnotatedClass/AnnotatedClassTestData/MyExtendedClass.php
+++ tests/fixtures/AnnotatedClass/AnnotatedClassTestData/MyExtendedClass.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace ElasticOdm\AnnotatedClass\AnnotatedClassTestData;
 
+use ElasticOdm\Mapping;
+
+/** @Mapping\Type */
 final class MyExtendedClass extends MyClass
 {
+	/**
+	 * @Mapping\BooleanType
+	 * @var bool
+	 */
+	private $isThisClassUseless;
+
     public function sayHiAndTryToConfuseClassGetter(): string
     {
         $confusingString = 'Class namespace';
